21a3f610fcb0e2aedc697a39636d2aadc858ac48
egorshv22 <shvedov124@gmail.com>
2021-11-23 18:59:18 +0300

    Add get_price_by_year function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 
+API = '82N6XECHCH9Z17R7'
+
 
 def get_price(symbols: list):
     """
     :param symbols: List of names of securities
     :return: List like [('name', price, 'latest trading day'), ('name2', price2, 'latest trading day2'), ...]
     """
-    API = '82N6XECHCH9Z17R7'
     prices = []
     for symbol in symbols:
         url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={API}'
@@ -19,3 +20,22 @@ def get_price(symbols: list):
 
 
 # print(get_price(['IBM', 'AAPL', 'DAX']))
+
+
+def get_price_by_years(stock):
+    """
+    :param stock: name of securities
+    :return: List like [(year, price), (year2, price2), ...]
+    """
+    res = []
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={stock}&outputsize=full&apikey={API}'
+    r = requests.get(url)
+    json = r.json()
+    data = json['Monthly Time Series']
+    keys = list(data.keys())
+    for i in range(0, len(keys), 12):
+        res.append((keys[i], data[keys[i]]['4. close']))
+    return res
+
+
+# print(get_price_by_years('IBM'))
